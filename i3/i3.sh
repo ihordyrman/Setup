3a913e3b8e39f0ac24e45a5640bbe7436b6a837f
Ihor <ihor.dyrman@outlook.com>
2025-07-26 16:08:54 +0300

    i3 config

---
+++ i3/i3.sh
@@ -0,0 +1,7 @@
+sudo apt-get install i3
+i3 --version
+
+sudo apt-get install i3-wm dunst i3lock i3status suckless-tools
+
+### check monitor name first and put it to the config
+xrandr --output eDP-1 --off

04082db16f42baa8ec7ae6fc03c434fbdd35c3fc
Ihor <ihor.dyrman@outlook.com>
2025-12-13 17:35:47 +0200

    nvim

---
+++ shell/nvim/lua/keymaps.lua
@@ -0,0 +1,11 @@
+-- [[ Keymaps ]]
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("n", "<C-Up>", "<Up>ddp<Up>", { desc = "Move line up" })
+vim.keymap.set("n", "<C-Down>", "ddp", { desc = "Move line down" })
+
+-- Clear search highlighting
+vim.cmd("noh") -- or vim.cmd.noh()
